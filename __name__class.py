f3c74bea6c424afd2ac76a526cfb34fd36f22008
Zyno16 <135590226+Zyno16@users.noreply.github.com>
2023-12-09 09:06:52 +0000

    Add files via upload

---
+++ __name__class.py
@@ -0,0 +1,16 @@
+class employee:
+    'this is employee class'
+    name = "empty name"
+    address ="empty address"
+
+class doctor:
+    pass
+
+class computer:
+    pass
+
+emp = employee()
+
+print(employee.__name__)
+print(doctor.__name__)
+print(computer.__name__)
